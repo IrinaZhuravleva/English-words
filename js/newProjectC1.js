d2d584d0ed1667ac094ec31f0f6b6082c9bd69f3
Irina Zhuravleva <iravizion@gmail.com>
2023-06-05 17:19:27 +0100

    Changes - duplicated words are removed

---
+++ js/newProjectC1.js
@@ -0,0 +1,67 @@
+const topics = [
+    'Environmental issues',
+    'Learning and education',
+    'Work and career',
+    'Character, attidude',
+    'Business and money',
+    'Culture and art',
+    'Sport, fitness, and health',
+    'Leisure and entertainment',
+    'Social issues, conflict, controversy',
+    'Politics and economy',
+    'Feelings and emotions',
+    'Travelling and tourism',
+    'Languages, books and communication',
+    'Science and technology',
+    'Family and relationships',
+    'Law and order',
+    'Media, fashion and celebrities',
+    'Conflicts',
+];
+
+// 'Science and technology',
+
+const sciTech = [
+    // 'power cut',
+    'acute digital addiction',
+    'to harness some technology',
+    'to be hailed as the most significant scientific breakthrough',
+    'pushing back the boundaries/frontiers of knowledge',
+];
+
+const leasure = [
+    'to induce a feeling of wellbeing',
+    'to be conducive to relaxation',
+    'mountaineering paraphernalia',
+    // 'sth profoundly altered the leisure',
+    'patterns of (some) society',
+];
+
+const business = [
+    'erratic (freelance) work',
+    // 'law of marginal returns',
+    'give smb an edge over competitors',
+    'a volatile year for the economy',
+    'to spot a gap in the market',
+];
+
+// What do you think is the most positive aspect of technology?
+// - I guess the opportunity to push back the frontiers of the accepter reality.
+// Discover the new norm every day - in household comfort, in education , in industrial production.
+// Even the personal boundaries - e.g., harnessing the technology to see the world.
+
+// - Where are you from?
+// From Krasnodar - that's newar the Southern part of Russia, in the vicinity of the Sochi - the premises 
+// for the Olympic Games -2014. - CLARIFICATION
+
+// I'm a bit of a tech geek, I guess. I'm always on the lookout for the latest gadget.
+
+// - How important do you think it is to speak more than one language?
+//Yes, I firmly believe it's indispensable part of a well-rounded education in the modern world.
+//There are really many contexts for one's success to derive from international communication. - REASON
+
+// - What do you enjoy most about learning English?
+//I take great pleasure in the opporunity to learn new things and to broaden my horizons.
+//Say, some professional publications in English put mew wise of more new teaching methods than just Russian prof media.
+// - EXAMPLE    
+
